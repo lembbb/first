eb04bdb8adcdcd1166459d1362b510cbfb6fbf46
lembbb <lemberg89@gmail.com>
2020-04-24 03:00:44 +0300

    - 3 commit

---
+++ script/bomj_script.js
@@ -0,0 +1,125 @@
+// let name = "roma";
+//
+// console.log(name);
+
+// let name = prompt("name");
+// let prizv = prompt("prizv")
+// let pobatkovi = prompt("pobat")
+//
+// let person = name + " " + prizv + " " + pobatkovi
+//
+// console.log(person);
+
+// let number1 = parseInt(prompt("1"));
+// let number2 = parseInt(prompt("1"));
+// let number3 = parseInt(prompt("1"));
+//
+// let fff = number3 + number2 + number1
+//
+// console.log(fff);
+
+// let num1 = parseInt(prompt("1"));
+// let num2 = parseInt(prompt("2"));
+//
+// let mat = Math.pow(num1,num2);
+//
+// console.log(mat);
+
+// let a = 100; let b = '100'; let c = true; let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// console.log(users[2].status);
+
+
+// // 1.Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// // При помощи оператора typeof убедитесь, что значения переменных принадлежат типам: string, number и  boolean.
+// let str = "Priv";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+//
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+
+// 6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
+// Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор возведения в степень ** ).
+//
+// let n = 3;
+// let m = 4;
+// let k = Math.pow(3,2) + Math.pow(4, 2)
+// console.log(Math.sqrt(k));
+
+
+
+// 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write, alert и console.log
+//
+// let l = "Hello world";
+// console.log(l);
+// document.write(l);
+// alert(l);
+
+// 8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).
+//
+// let f = "roma m m";
+// let v = 222;
+// let h = "bbb"
+// alert(f + "\n" + v + "\n" + h)
+
+
+// 9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+//     Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+//   Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+//     Вывести в документ содержимое переменной concatenation спомощью document.write
+//
+// 10. Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+// // 11. Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?
+// console.log(parseInt("3.14"));
+// console.log(parseInt("-7.875"));
+// console.log(parseInt("435"));
+// console.log(parseInt("Вася"));
+
+// 12.  С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные, которые будут использоваться далее, повторите код ниже
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+//
+// 13. С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата при помощи метода alert
+// 14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и возраст, а вам не обходимо вывести строку такого вида
+// Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+
+// lesson1 end ___________________________________________________________________________
+
+
+//
+
+
+
+//
+
+
+// lesson2 start __________________________________________________________________________
